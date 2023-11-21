b314905ffaad634b91e9f00414bc54d1c6926804
unknown <nursultanr585@gmail.com>
2023-11-21 22:44:47 +0600

    html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+
+      </div>
+      <div className="content">
+        <div>
+
+        </div>
+        <div>
+          <h1>Make easier your crypto transactions.</h1>
+          <p>Our cryptocurrency offers a fast, secure, and affordable alternative for all of your financial needs. Join the cryptocurrency revolution today and take control of your financial future. Sign up now to start using our platform for yourself.</p>
+        </div>
+      </div>
+      <div className="content_partners">
+
+      </div>
+      <div className="content_crypto">
+        <div>
+
+          <h1>Best crypto currently</h1>
+        </div>
+        <div>
+          <div className="crypto_container">
+            <div className="crypto_block">
+              <img src=""/>
+              <h1>Bitcoin</h1>
+              <p>31,020.564 $</p>
+              <button>buy it now</button>
+            </div>
+            <div className="crypto_block">
+              <img src=""/>
+              <h1>Ethereum</h1>
+              <p>31,020.564 $</p>
+              <button>buy it now</button>
+            </div>
+            <div className="crypto_block">
+              <img src=""/>
+              <h1>Ripple</h1>
+              <p>31,020.564 $</p>
+              <button>buy it now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="content_banner_1">
+        <div>
+          <img/>
+        </div>
+        <div>
+          <h1>Track your earnings and cryptocode made it easy</h1>
+          <p>Nulla dui ultrices sed nam ligula dignissim tellus. Sem semper lorem facilisis ut amet tincidunt adipiscing maecenas.</p>
+          <button className="">Get started</button>
+        </div>
+        
+      </div>
+      <div className="content_banner_2">
+        <div>
+          <h1>Trust us due to our safe and convenient deposit</h1>
+          <p>Tristique morbi scelerisque vitae. Nulla dui ultrices sed nam ligula dignissim tellus semper lorem facilisis ut. Amet tincidunt adipiscing varius maecenas adipiscing. </p>
+          <button>Know more</button>
+        </div>
+        <div>
+          <img src=""/>
+        </div>
+      </div>
+      <div className="footer_contact">
+        <div>
+          <h1>Subscribe & get news updates</h1>
+        </div>
+        <div>
+        Duis massa posuere commodo sed tortor ultrices accumsan augue. Aliquet bibendum placerat vivamus diam ullamcorper vitae sapien auctor ut. Suspendisse donec id aenean diam dolor nec vitae massa id. Velit nullam consequat orci amet fames sed. Amet tincidunt adipiscing varius maecenas adipiscing. 
+
+        </div>
+        <div>
+          <input/>
+        </div>
+      </div>
+      <div className="footer">
+
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
